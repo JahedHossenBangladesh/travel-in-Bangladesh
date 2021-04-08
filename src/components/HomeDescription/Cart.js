4794f22fd1ceda67f085287a3f72c3982d9d1948
JahedHossenBangladesh <jahedhossen4358.com>
2021-04-09 01:04:57 +0600

    cart minimization is added

--- src/components/HomeDescription/Cart.js
+++ src/components/HomeDescription/Cart.js
@@ -1,22 +1,30 @@
 import {
   faArrowRight,
-  faStar
+
+
+  faChevronDown, faChevronUp, faStar
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from 'react';
+import React, { useState } from 'react';
+import { Collapse } from "react-bootstrap";
+
+
+
 const Cart = () => {
+
+const [open, setOpen] = useState(true);
     return (
       <div className="container border">
         <h6>$34/night</h6>
         <p className="text-secondary">
           <FontAwesomeIcon icon={faStar} color="green" /> 4.9(20 reviews)
         </p>
+        <p>Date</p>
         <div className="row pl-3">
-          
           <div className="col-sm-10 border">
             <h5 className="text-secondary">
               4/12/2021
-              <span className="pl-5 pr-5">
+              <span className="pl-5 pr-5 pt-5 pd-5">
                 <FontAwesomeIcon icon={faArrowRight} />
               </span>
               8/12/2021{" "}
@@ -24,6 +32,22 @@ const Cart = () => {
           </div>
           <div className="col-sm-1"></div>
         </div>
+        <p>Guests</p>
+        <div className="row border ml-1 mr-1">
+          <div className="col-sm-10">3 Guests</div>
+          <div className="col-sm-2">
+            <FontAwesomeIcon
+              icon={open ? faChevronUp : faChevronDown}
+              size="1x"
+              onClick={() => setOpen(!open)}
+              aria-controls="example-collapse-text"
+              aria-expanded={open}
+            />
+          </div>
+        </div>
+<Collapse in={open}>
+  <p>This is me</p>
+</Collapse>
       </div>
     );
 };
